6da1a15c1e97b0b7f89fa5225098fb7100a9cdd7
Luke Moderwell <luke.moderwell@gmail.com>
2018-11-24 22:19:23 -0600

    basic registration

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,36 +1,41 @@
 <template>
   <form class="c-form" id="register">
     <Input 
-      v-model="firstName"
+      v-model="user.firstName"
       type="text" 
       placeholder="John" 
       label="First Name" />
     <Input 
-      v-model="lastName"
+      v-model="user.lastName"
       type="text" 
       placeholder="Doe" 
       label="Last Name" />
     <Input 
-      v-model="email"
+      v-model="user.email"
       type="email" 
       placeholder="me@name.com" 
       label="Email" />
     <Input 
-      v-model="password" 
+      v-model="user.password" 
       type="password" 
       label="Password" 
       v-bind:minlength="4" />
     <Input 
-      v-model="confirmPassword" 
+      v-model="user.confirmPassword" 
       type="password" 
       label="Confirm Password" 
       v-bind:minlength="4" />
     <input type="submit" v-on:click="handleSubmit"/>
-    
   </form>
 </template>
 
 <script>
+import Airtable from "airtable";
+Airtable.configure({
+  endpointUrl: "https://api.airtable.com",
+  apiKey: process.env.VUE_APP_AT_API_KEY
+});
+const base = Airtable.base(process.env.VUE_APP_AT_BASE);
 // @ is an alias to /src
 import Input from "@/components/Input.vue";
 
@@ -38,11 +43,14 @@ export default {
   name: "Register",
   data() {
     return {
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: '',
-      confirmPassword: ''
+      user: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        image: "",
+        password: "",
+        confirmPassword: ""
+      }
     };
   },
   components: {
@@ -51,7 +59,46 @@ export default {
   methods: {
     handleSubmit: function(event) {
       event.preventDefault();
-      console.log(`email: ${this.email}, password: ${this.password}`);
+      this.createUser(this.user);
+    },
+    checkPassword: function(password, confirm) {
+      let result = false;
+      if (password !== '') {
+        password == confirm 
+          ?  result = true : result = false
+      }
+      return result;
+    },
+    formError: function(user) {
+      let errors = [];
+      for (var key in user) {
+        user[key] == '' ? errors.push(key) : '';
+      }
+      alert(String(errors));
+    },
+    createUser: function(data) {
+      if (this.checkPassword(data.password, data.confirmPassword)) {
+        base("Users").create({
+          email: data.email,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          image: [
+            {
+              url: "https://dl.airtable.com/hyJetILvQ22MlnH5PsYN_luke.jpg"
+            }
+          ],
+          password: data.password
+        },
+        function(err, record) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          console.log(record.getId());
+        });
+      } else {
+        this.formError(this.user);
+      }
     }
   }
 };
